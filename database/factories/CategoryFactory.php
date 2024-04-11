e8e39969fa4747eb9909160efb62bd1908da1a53
Heisenberg <heisenberg@kontramundo.com>
2024-04-11 11:58:59 -0600

    Migrations and seeders are created

---
+++ database/factories/CategoryFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Category>
+ */
+class CategoryFactory extends Factory
+{
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        // the category name should be unique
+        $name = $this->faker->unique()->word();
+
+        return [
+            'name' => $name,
+            // generate slug from category title
+            'slug' => Str::slug(
+                title: $name,
+            ),
+        ];
+    }
+}
---
+++ database/factories/PostFactory.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $title = $this->faker->unique()->sentence();
+
+        return [
+            'title' => $title,
+            'slug' => Str::slug(
+                title: $title,
+            ),
+            'content' => $this->faker->paragraph(),
+            'published' => $this->faker->boolean(
+                chanceOfGettingTrue: 85,
+            ),
+            'category_id' => Category::inRandomOrder()->first()->id,
+            'user_id' => User::inRandomOrder()->first()->id
+        ];
+    }
+}
